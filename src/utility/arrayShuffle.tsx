b233819b978fe738c7ee46ad63d7462561df48ee
Robert <robertgryko@outlook.com>
2022-11-03 14:29:40 +0100

    changed input type to generic

--- src/utility/arrayShuffle.tsx
+++ src/utility/arrayShuffle.tsx
@@ -1,5 +1,5 @@
 //  Durstenfeld shuffle
-function shuffle(array: any[]) {
+function shuffle<T>(array: T[]) {
   let tempArray = array;
   for (let i = tempArray.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
