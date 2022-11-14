5dafb9e6ce140e5f9f2175cea17c83d80f1c91ca
Robert <robertgryko@outlook.com>
2022-11-14 09:52:00 +0100

    strict mode on

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { store } from "./store/store";
@@ -8,12 +9,11 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  // <React.StrictMode>
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
-
-  // </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 );
