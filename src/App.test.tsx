bd4cf082176a97eff5cbc5d1f352d706326973e4
Robert <robertgryko@outlook.com>
2022-11-22 19:35:35 +0100

    got rid of act error

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,10 @@
-import { act, render } from "@testing-library/react";
+import {
+  act,
+  render,
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 import "@testing-library/jest-dom";
 import App from "./App";
 import { store } from "./store/store";
@@ -6,43 +12,36 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 
 describe("App component", () => {
-  test("hides side menu and shows burger menu when width is below 490px", () => {
-    const { container } = render(
+  beforeEach(async () => {
+    global.fetch = jest.fn().mockResolvedValue({
+      json: () => {},
+    });
+    render(
       <Provider store={store}>
         <BrowserRouter>
           <App />
         </BrowserRouter>
       </Provider>
     );
+    await waitFor(() =>
+      screen.getByText(`Loading error. Try refresh. Request failed`)
+    );
+  });
+  const fireResize = (width: number) => {
+    window.innerWidth = width;
+    window.dispatchEvent(new Event("resize"));
+  };
+  const sidebarMenu = document.getElementsByClassName("sidebar");
+  const burgerMenu = document.getElementsByClassName("bm-burger-button");
 
-    const fireResize = (width: number) => {
-      window.innerWidth = width;
-      window.dispatchEvent(new Event("resize"));
-    };
-
+  test("hides side menu and shows burger menu when width is below 490px", async () => {
     act(() => fireResize(489));
-    const sidebarMenu = container.getElementsByClassName("sidebar");
-    const burgerMenu = container.getElementsByClassName("bm-burger-button");
     expect(sidebarMenu.length).toEqual(0);
     expect(burgerMenu.length).toBeGreaterThan(0);
   });
 
   test("hides burger menu and shows side menu when width is above 490px", () => {
-    const { container } = render(
-      <Provider store={store}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </Provider>
-    );
-    const fireResize = (width: number) => {
-      window.innerWidth = width;
-      window.dispatchEvent(new Event("resize"));
-    };
-
     act(() => fireResize(591));
-    const sidebarMenu = container.getElementsByClassName("sidebar");
-    const burgerMenu = container.getElementsByClassName("bm-burger-button");
     expect(sidebarMenu.length).toBeGreaterThan(0);
     expect(burgerMenu.length).toEqual(0);
   });
